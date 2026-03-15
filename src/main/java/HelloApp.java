584952ade62364fd0b8bbd104c9fbfc9678763a5
yashmita09 <yashmitanandanwar@gmail.com>
2026-03-15 18:35:37 +0530

    Added UC2

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,16 @@
-public class HelloApp{
-    public static void main(String[] srgs){
-        System.out.println("Hello World");
+import java.util.Scanner;
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+        scanner.close();
     }
 }
